07a2aaf620e77db6fdf66ea8e71afd369434217a
Julio Machado <jsmacbusiness@gmail.com>
2023-08-20 15:26:49 -0300

    Anatomia das Classes - 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,10 +2,27 @@ public class MinhaClasse {
     public static void main (String [] args) {
         System.out.print( "Olá Mundo!!" );
 
+        // Variaveis
         int ano = 2023;
         ano = 2024;
         final String BR = "Brasil";
 
         // BR = "Brazil" (colocar final na variavel em JAVA, funciona igual const para JAVASCRIPT.)
+
+        String meuNome = "Júlio";
+        int anoFabricacao = 2022;
+        boolean verdadeira = true;
+        anoFabricacao = 2018;
+
+        // Métodos
+        String primeiroNome = "Julio";
+        String segundoNome = "Machado";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    // Métodos
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método" + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
