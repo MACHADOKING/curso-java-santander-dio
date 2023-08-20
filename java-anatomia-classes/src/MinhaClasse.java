a595ba0a28d48c83c8baefa44b72253d55798a1f
Julio Machado <jsmacbusiness@gmail.com>
2023-08-20 14:43:13 -0300

    Anatomia das Classes - 2

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,11 @@
 public class MinhaClasse {
     public static void main (String [] args) {
         System.out.print( "Olá Mundo!!" );
+
+        int ano = 2023;
+        ano = 2024;
+        final String BR = "Brasil";
+
+        // BR = "Brazil" (colocar final na variavel em JAVA, funciona igual const para JAVASCRIPT.)
     }
 }
