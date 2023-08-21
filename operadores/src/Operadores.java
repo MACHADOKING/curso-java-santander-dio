c304cd723893a5bc0d2830a9bb8af33bc088d6e1
Julio Machado <jsmacbusiness@gmail.com>
2023-08-21 15:12:08 -0300

    Operadores - 3

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,7 +1,13 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
         String nomeCompleto = "Linguagem" + "JAVA";
-        System.out.println(nomeCompleto);
-        
+            System.out.println(nomeCompleto);
+        int numero = 5;
+            System.out.println(- numero);
+            System.out.println(numero);
+        numero = - numero;
+            System.out.println(numero);
+        numero = numero * -1;
+            System.out.println(numero);
     }
 }
