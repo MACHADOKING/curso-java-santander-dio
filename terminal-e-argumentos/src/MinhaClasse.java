cdbc2714e59b8a4a9d43e4b1196c49ac539c7ca7
Julio Machado <jsmacbusiness@gmail.com>
2023-08-21 18:19:29 -0300

    Terminal e Argumentos - 2

---
+++ terminal-e-argumentos/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    // Terminal e Argumentos 2
+    /* Executei esse programa via terminal */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi, fui executado pelo terminal");
+    }
+}
