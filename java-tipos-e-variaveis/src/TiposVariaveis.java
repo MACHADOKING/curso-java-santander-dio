74ea1ee31fb60603fb0d0051587ecb720989fa7b
Julio Machado <jsmacbusiness@gmail.com>
2023-08-21 14:12:49 -0300

    Tipos e Variaveis - 3

--- java-tipos-e-variaveis/src/TiposVariaveis.java
+++ java-tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+            System.out.println(salarioMinimo);
+        short numeroCurto = 1;
+            System.out.println(numeroCurto);
+        int numeroNormal = numeroCurto;
+            System.out.println(numeroNormal);
+        short numeroCurto2 = (short) numeroNormal;
+            System.out.println(numeroCurto2);
     }
 }
