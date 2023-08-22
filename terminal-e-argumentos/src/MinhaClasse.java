72117d78165b9454a2f3c570a9f56c6dc2e39747
Julio Machado <jsmacbusiness@gmail.com>
2023-08-22 14:10:28 -0300

    Feat

--- terminal-e-argumentos/src/MinhaClasse.java
+++ terminal-e-argumentos/src/MinhaClasse.java
@@ -3,5 +3,9 @@ public class MinhaClasse {
     /* Executei esse programa via terminal */
     public static void main(String[] args) throws Exception {
         System.out.println("Oi, fui executado pelo terminal");
+
+        int numero1 = 1;
+        String numero2 = "2";
+         System.out.println(numero1+numero2);
     }
 }
