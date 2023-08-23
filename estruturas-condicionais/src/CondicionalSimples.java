316bf5c1d12d569a47d332cb39a4bfd065c25e7d
Julio Machado <jsmacbusiness@gmail.com>
2023-08-23 15:34:57 -0300

    Condicional Simples

---
+++ estruturas-condicionais/src/CondicionalSimples.java
@@ -0,0 +1,13 @@
+public class CondicionalSimples {
+    public static void main(String[] args) throws Exception {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println(saldo);
+        } else {
+            System.out.println("A operação não pode ser executada (Saldo insuficiente)");
+        }
+    }
+}
