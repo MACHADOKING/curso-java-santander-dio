352faf245be515ba7fb5c5e47ebd4a2cbbcbc0c3
Julio Machado <jsmacbusiness@gmail.com>
2023-08-28 16:23:33 -0300

    Feat

---
+++ programacao-orientada-a-objetos/src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	// método construtor
+	// o nome deverá ser igual ao nome da classe
+	public Pessoa (String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+	
+	//...
+	//getters
+	//setters
+}
