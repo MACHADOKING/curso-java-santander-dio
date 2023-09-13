52380108a851b30502c5cdd5ca7ea9d731217550
Julio Machado <jsmacbusiness@gmail.com>
2023-09-13 13:48:36 -0300

    Expondo Endpoints REST e Testando a API

---
+++ apirest-cloud/src/main/java/dio/apirestcloud/controller/UserController.java
@@ -0,0 +1,39 @@
+package dio.apirestcloud.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import dio.apirestcloud.domain.model.User;
+import dio.apirestcloud.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}") // GET
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping // POST
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(userCreated.getId()).toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
